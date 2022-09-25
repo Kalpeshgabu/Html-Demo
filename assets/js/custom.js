6182978360c395a7ec971442de826246707bd707
Kalpeshgabu <Kalpeshgabu.kg@gmail.com>
2022-09-25 18:16:41 +0530

    changes done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -22,5 +22,28 @@ function openMenu() {
   element.classList.add("activeclass");
 }
 function closeMenu() {
-  element.classList.remove("activeclass")
+  element.classList.remove("activeclass");
 }
+
+
+/*Fetch Api Calling */
+
+console.log('This is my tutorial')
+
+const myBtn = document.getElementById('my-btn');
+
+const content = document.getElementById('content');
+
+function getData() {
+  url = "Interview.txt";
+  console.log("Started getData");
+  fetch(url).then((response)=> {
+    return response.text();
+  }).then((data)=>{
+    console.log(data);
+  })
+}
+console.log("Before runnig getData");
+getData();
+console.log("After runnig getData");
+ 
